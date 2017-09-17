66779a38b185be97130b21d6d107f439b16e0acc
rcsv <Awane.Tomohiro@gmail.com>
2017-09-17 19:28:50 +0900

    Implememnt Tutorial: PHP 初めてのフレームワーク Laravel 5.3, 5.4, 5.4

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Http\Request ;
+use App\Book ;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    //
+});
+
+Route::post('/book', function (Request $request){
+
+	//
+	// just a stab.
+	// --------------------------------------------------------------------
+
+});
+
+Route::delete('/book/{id}', function() {
+
+	//
+	// DELETE method
+	// --------------------------------------------------------------------
+});
