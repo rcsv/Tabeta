5631aaadb97fe6c158fd92eccc0df56e9d8570bb
rcsv <Awane.Tomohiro@gmail.com>
2017-09-20 22:24:21 +0900

    Add SCAFFOLDING

--- config/app.php
+++ config/app.php
@@ -177,6 +177,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        Laralib\L5scaffold\GeneratorsServiceProvider::class,
     ],
 
     /*
